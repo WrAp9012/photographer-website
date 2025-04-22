1f24211bfc327bed3d0c75c1d5cd95dbec751eb7
WrAp9012 <szymonburda19@gmail.com>
2025-04-22 12:22:25 +0200

    Create models and base page for display data

--- photographer_site/main_app/models.py
+++ photographer_site/main_app/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
 # Create your models here.
+
+
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(status=Post.Status.PUBLISHED)
+
+
+# TODO: Add Images field to database
+class Post(models.Model):
+
+    class Status(models.TextChoices):
+        DRAFT = "DF", "Draft"
+        PUBLISHED = "PB", "Published"
+
+    title = models.CharField(max_length=250)
+    body = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="post")
+    created = models.DateTimeField(default=timezone.now)
+    publish = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    status = models.CharField(
+        max_length=2, choices=Status.choices, default=Status.DRAFT
+    )
+
+    objects = models.Manager()
+    published = PublishedManager()
+
+    class Meta:
+        ordering = ["-publish"]
+        indexes = [models.Index(fields=["-publish"])]
+
+    def __str__(self):
+        return f"Title: {self.title}"
--- photographer_site/main_app/views.py
+++ photographer_site/main_app/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
+
+from main_app.models import Post
 
 # Create your views here.
+
+
+def post_list(request):
+    post_list = Post.published.all()
+    paginator = Paginator(post_list, 3)
+    page_number = request.GET.get("page", 1)
+
+    try:
+        post = paginator.page(page_number)
+    except PageNotAnInteger:
+        post = paginator.page(1)
+    except EmptyPage:
+        post = paginator.page(paginator.num_pages)
+
+    return render(request, "main_app/post/list.html", {"post": post})
